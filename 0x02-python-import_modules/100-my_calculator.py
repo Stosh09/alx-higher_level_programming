7cc13a3136266a4d43479dd4b971ece8f3efbff9
Stosh09 <stoshochieng@gmail.com>
2023-08-12 02:23:47 +0300

    Help build my calculator by importing from calculator_1.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
+
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -16,3 +17,4 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+
