0649dbf6f72b3317a3861bba5dc70216d12165e1
Stosh09 <stoshochieng@gmail.com>
2023-09-18 21:59:59 +0300

    define public method update for class square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,7 +26,7 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
-        
+
     def update(self, *args, **kwargs):
         """Update the Square.
 
