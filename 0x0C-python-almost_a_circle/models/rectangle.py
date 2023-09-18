12f7a93e3483265172d0cd060328c072f761a489
Stosh09 <stoshochieng@gmail.com>
2023-09-18 21:31:49 +0300

    define method Area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines a rectangle class."""
 from models.base import Base
 
@@ -80,3 +78,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.width * self.height
