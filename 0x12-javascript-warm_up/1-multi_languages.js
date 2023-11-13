edbb4c2816d388e724705d8ec6d75f91a08fb7da
Stosh09 <stoshochieng@gmail.com>
2023-11-13 08:50:18 +0300

    Print content from 3 different variables

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let var1 = 'C is fun';
-let var2 = 'Python is cool';
-let var3 = 'Javascript is amazing';
+const var1 = 'C is fun';
+const var2 = 'Python is cool';
+const var3 = 'Javascript is amazing';
 console.log(var1);
 console.log(var2);
 console.log(var3);
