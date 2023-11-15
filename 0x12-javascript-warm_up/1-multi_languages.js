f1bc14ce1a996096e06dea50e67c7f3c79137c9d
Stosh09 <stoshochieng@gmail.com>
2023-11-15 12:38:00 +0300

    Script that prints a square

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const var1 = 'C is fun';
-const var2 = 'Python is cool';
-const var3 = 'Javascript is amazing';
-console.log(var1);
-console.log(var2);
-console.log(var3);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
