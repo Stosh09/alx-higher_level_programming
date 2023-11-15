0b6029e9da1779e7b94e4dbdf732e84b6484a3c0
Stosh09 <stoshochieng@gmail.com>
2023-11-16 02:46:41 +0300

    Instantiate charprint that prints square using C or X

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ module.exports = class Square extends require('./5-square') {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++);
+      for (let i = 0; i < this.height; i++)
       console.log(c.repeat(this.width));
     }
   }
