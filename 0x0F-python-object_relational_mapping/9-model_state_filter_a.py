1f12b8522b191644356904bc277f98a4035f9383
Stosh09 <stoshochieng@gmail.com>
2023-11-24 21:07:59 +0300

    List all states in db starting with

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+script that lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+                            argv[1], argv[2], argv[3]))
+    session = Session(bind=engine)
+    states = session.query(State).filter(State.name.like("%a%"))
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
