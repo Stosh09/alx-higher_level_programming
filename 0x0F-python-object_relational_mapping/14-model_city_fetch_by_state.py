cc57ac32ba27eb7c98159c2f2af4566962f35d1a
Stosh09 <stoshochieng@gmail.com>
2023-11-24 21:42:05 +0300

    List all city objects in db

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-
-"""
-script that deletes all State objects with a name
-containing the letter a from the database hbtn_0e_6_usa
-"""
-
+# Lists all City objects from the database hbtn_0e_14_usa.
+# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
+#                                          <mysql password> /
+#                                          <database name>
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sys import argv
-from model_state import Base, State
-
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-                            argv[1], argv[2], argv[3]))
-    session = Session(bind=engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    states_to_delete = session.query(State)\
-                              .filter(State.name.like("%a%"))\
-                              .delete(synchronize_session='fetch')
-
-    session.commit()
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
